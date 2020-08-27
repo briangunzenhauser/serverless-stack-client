80b04397d59d78347c7e50ca33465cd8d8f7a51c
Brian Gunzenhauser <brian.gunzenhauser@gmail.com>
2020-08-26 20:59:57 -0400

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "boxandpixel-serverless-app-notes"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://4cfa6amt69.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_Cv9tSJdoY",
+        APP_CLIENT_ID: "3mdkuan2q0m17t3ragedjvjah8",
+        IDENTITY_POOL_ID: "us-east-1:67998fa2-9c43-4e0f-a5d8-0687a7889e6a"
+    }
+};
